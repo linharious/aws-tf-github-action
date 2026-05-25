4f0fa8c3c9bd4facc6f437489d52386bf2dd9307
linharious <linh.t.tn14@gmail.com>
2026-05-25 12:32:48 -0400

    fix formatting -,-

--- src/main.tf
+++ src/main.tf
@@ -2,17 +2,17 @@ terraform {
   required_version = ">=0.13.0"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~>3.0"
+      source  = "hashicorp/aws"
+      version = "~>3.0"
     }
   }
 
   backend "s3" {
-    bucket = "tf-state-backend-test-9875"
-    key = "global/s3/terraform.tfstate"
-    region = "us-west-2"
+    bucket         = "tf-state-backend-test-9875"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-west-2"
     dynamodb_table = "terraform-state-locking"
-    encrypt = true
+    encrypt        = true
   }
 }
 
@@ -21,14 +21,14 @@ provider "aws" {
 }
 
 module "tf-state" {
-    source = "./modules/tf-state"
-    bucket_name = "tf-state-backend-test-9875"
+  source      = "./modules/tf-state"
+  bucket_name = "tf-state-backend-test-9875"
 }
 
 module "vpc-infra" {
-  source = "./modules/vpc"
-  vpc_cidr = local.vpc_cidr
-  availability_zones = local.availability_zones
-  public_subnet_cidrs = local.public_subnet_cidrs
+  source               = "./modules/vpc"
+  vpc_cidr             = local.vpc_cidr
+  availability_zones   = local.availability_zones
+  public_subnet_cidrs  = local.public_subnet_cidrs
   private_subnet_cidrs = local.private_subnet_cidrs
 }
--- src/modules/tf-state/tf-state.tf
+++ src/modules/tf-state/tf-state.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
   force_destroy = true
 }
 
@@ -20,9 +20,9 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name = "terraform-state-locking"
+  name         = "terraform-state-locking"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
   attribute {
     name = "LockID"
     type = "S"
