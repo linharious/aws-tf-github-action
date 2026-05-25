8a1bd5bbb0bac8df5c8b94d3ab3b5392056fb31c
linharious <linh.t.tn14@gmail.com>
2026-05-25 12:37:47 -0400

    modify availability zones

--- src/local.tf
+++ src/local.tf
@@ -1,5 +1,5 @@
 locals {
-  availability_zones = ["us-east-1a", "us-east-1b"]
+  availability_zones = ["us-west-2a", "us-west-2b"]
 
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidrs  = ["10.0.0.0/24", "10.0.1.0/24"]
