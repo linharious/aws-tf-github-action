58fac593e668440f6a9217663536d338931c8e25
linharious <linh.t.tn14@gmail.com>
2026-05-25 12:22:51 -0400

    add vpc and github actions

--- src/main.tf
+++ src/main.tf
@@ -24,3 +24,11 @@ module "tf-state" {
     source = "./modules/tf-state"
     bucket_name = "tf-state-backend-test-9875"
 }
+
+module "vpc-infra" {
+  source = "./modules/vpc"
+  vpc_cidr = local.vpc_cidr
+  availability_zones = local.availability_zones
+  public_subnet_cidrs = local.public_subnet_cidrs
+  private_subnet_cidrs = local.private_subnet_cidrs
+}
